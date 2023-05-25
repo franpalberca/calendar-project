9161cc7d555e2560adbcbb3cc38231ab466e31c5
kevinPolania <kevin.narvaez.rc@gmail.com>
2023-05-25 15:40:30 +0100

    creating variables

---
+++ src/funcionalities/yearUpdate.ts
@@ -0,0 +1,9 @@
+function updateYear() {
+    const currentYear = new Date().getFullYear();
+    const yearElement = document.getElementById('yearUpdateFooter');
+    if (yearElement) {
+      yearElement.textContent = currentYear.toString();
+    }
+  }
+  window.addEventListener('DOMContentLoaded', updateYear);
+  
