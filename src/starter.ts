beb2000fc53eb6efe5a5f276f4628aba8c906697
Fran Pérez Alberca <fran.perez.alberca@gmail.com>
2023-05-30 12:03:18 +0200

    Adding new modals

--- src/starter.ts
+++ src/starter.ts
@@ -6,8 +6,7 @@ import { restartEventForm } from './funcionalities/restartEventForm.js';
 import { recreateEvents } from './funcionalities/recreateEvents.js';
 import { changeThemeAddEvent} from './changesTheme/changeTheme.js';
 import { checkReminders } from './funcionalities/checkReminder.js';
-import { createEvents } from './funcionalities/howTo.js';
-import { deleteEvents } from './funcionalities/howTo.js';
+import { howToCreateEvents, howToDeleteEvents } from './funcionalities/howTo.js';
 import{logInBtnClick} from './sign-In/signIn.js'
 
 window.onload = () => {
@@ -21,8 +20,8 @@ window.onload = () => {
     changeThemeAddEvent();
     setInterval(checkReminders, 10000);
     recreateEvents();
-    createEvents();
-    deleteEvents();
+    howToCreateEvents();
+    howToDeleteEvents();
 
 
 
